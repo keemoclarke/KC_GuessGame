6ebb702c87b1e5365af26f476c6d0d4a82f47318
Keemo Clarke <keemocla@gmail.com>
2018-03-16 17:10:51 -0400

    added .cpp

---
+++ GuessGame.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>  // for rand()
+#include <ctime>
+
+using namespace std;
+
+int main(){
+
+srand(time(0));  // to generate truly random numbers
+
+int number = 1 + (rand() % 100);  // random number between [1,100]
+
+int input;
+
+cout << "\nGuess a number between 1 and 100: ";
+cin >> input;
+
+while (input !=number) {     // Keep asking until user gets correct number
+
+	if (input > number){
+		cout << " \n\nToo high. Try again: ";
+		cin >> input;
+	}
+
+	else{
+		cout << " \n\nToo low. Try again: ";
+		cin >> input;
+	}
+
+}
+
+
+
+cout << "\n\n\nCongrats! You're a winner!!!.\n\n";
+
+	return 0;
+}
